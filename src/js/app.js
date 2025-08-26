2b1a4f3989c36ed8e3fe5b5a94a680a0ac6ca011
hashhex <hashex@yandex.com>
2025-08-26 15:53:47 +0500

    clean up

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+import { Events } from './events.js'
+import { Sliders } from './sliders.js'
+import { Utils } from './utils.js'
+
+const App = () => {
+    Events()
+    Sliders()
+    Utils()
+}
+
+App()
